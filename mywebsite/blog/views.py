54cd7500b24d3014e347641b949acf9b0992c34a
fancyydev <davidfregosoleon12@gmail.com>
2024-10-14 08:11:03 -0600

    Se agrego la paginacion a la pagina web

--- mywebsite/blog/views.py
+++ mywebsite/blog/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def render_posts(request):
+    
     posts = Post.objects.filter(type='escritos')
     return render(request, 'posts.html', {'posts': posts})
 
 def list_written_posts(request):
-    posts = Post.objects.filter(type='escritos')
+    post_list = Post.objects.filter(type='escritos')
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)    
+    
     return render(request, 'posts.html', {'posts': posts})
 
 def list_recomendation_posts(request):
-    posts = Post.objects.filter(type='recomendaciones')
+    post_list = Post.objects.filter(type='recomendaciones')
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    
     return render(request, 'posts.html', {'posts': posts})
 
 def list_art_posts(request):
